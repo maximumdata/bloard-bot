c5e5e3a55e6f99e1d8a536caa4df8d55a5534d4b
Mike Dettmer <mike@mikedettmer.com>
2020-07-02 17:11:24 -0400

    feat() - makes bloardman stop saying the r-word

--- services/filterMessageContent.js
+++ services/filterMessageContent.js
@@ -1,3 +1,3 @@
 export default function filterMessageContent(message) {
-	return message.content.replace(/(\<.*?\,)/g, '').replace(/([!]+)/g, '').replace(/(\<.*?>)/g, '').trim();
+	return message.content.replace(/retarded/gi, 'insanely smart').replace(/retard/gi, 'genius').replace(/(\<.*?\,)/g, '').replace(/([!]+)/g, '').replace(/(\<.*?>)/g, '').trim();
 }
