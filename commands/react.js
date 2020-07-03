d0d39c4a7d8dad802a8b830f06133e70c619e0f5
Mike Dettmer <mike@mikedettmer.com>
2020-07-02 21:15:12 -0400

    feat() - adds !react <word> to react to last message in channel

--- commands/react.js
+++ commands/react.js
@@ -12,25 +12,47 @@ const emojis = {
     '!': '❗', '?': '❓',
 };
 
+const createWordArray = word => word.toLowerCase().split('');
+
+const reactToMessage = async (message, word) => {
+	const wordArray = createWordArray(word);
+	for (let x = 0; x < wordArray.length; x++) {
+		const letter = wordArray[x];
+		const emoji = emojis[letter];
+		if (emoji) {
+			try {
+				await message.react(emoji);
+			} catch (e) {
+				console.error(e);
+			}
+		} else {
+			continue;
+		}
+	}
+};
+
 export default {
 	name: 'react',
 	aliases: [],
 	description: 'Attempt to add reactions to a given message',
 	usage: '<Link or ID of message to react to> <word to spell out in reactions>',
 	execute: async function (message, args) {
-		const mes = args[0];
-		const what = mes.split('/');
-		const huh = what.length > 1 ? what[what.length - 1] : mes;
-		const word = args[1].toLowerCase().split('');
-		try {
-			const m = await message.channel.messages.fetch(huh);
-			for (let x = 0; x < word.length; x++) {
-				const letter = word[x];
-				const emoji = emojis[letter];
-				await m.react(emoji);
+		if (args.length > 1) {
+			const mes = args[0];
+			const what = mes.split('/');
+			const huh = what.length > 1 ? what[what.length - 1] : mes;
+			const word = args[1];
+			try {
+				const m = await message.channel.messages.fetch(huh);
+				await reactToMessage(m, word);
+			} catch (error) {
+				console.error(error);
 			}
-		} catch (error) {
-			console.error(error);
+		} else {
+			const word = args[0];
+			const twoMessages = await message.channel.messages.fetch({ limit: 2 });
+			const m = twoMessages.last();
+			await reactToMessage(m, word);
 		}
 	}
 };
