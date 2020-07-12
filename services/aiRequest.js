d95a943186fbec2a0a85d3309a16c82158fe9537
Mike Dettmer <mike@mikedettmer.com>
2020-07-12 01:56:30 -0400

    stops bloardman from replying to himself endlessly

--- services/aiRequest.js
+++ services/aiRequest.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default async function aiRequest(message, INFERKIT_API_KEY) {
+	if (message.author.username === 'bloardman') return;
     const { content, author: { username } } = message;
     const INFERKIT_URL = 'https://api.inferkit.com/v1/models/standard/generate';
     const conversationStructure = `bloardman: what would you like to talk about?
