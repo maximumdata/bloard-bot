62bc123025723b457f0f0733302e9e4754c3dfbe
Mike Dettmer <mike@mikedettmer.com>
2020-07-29 22:34:24 -0400

    feat() - adds post functionality from moog!

---
+++ commands/post.js
@@ -0,0 +1,13 @@
+import bloardPost from '../services/BloardPost';
+
+export default {
+	name: 'post',
+	aliases: [],
+	description: 'fetch a screenshot of a bloard post by given user',
+	usage: '!post <bloard username>',
+	execute: async function (message, args) {
+    message.channel.startTyping();
+    await bloardPost(message, args);
+    return message.channel.stopTyping()
+	}
+};
