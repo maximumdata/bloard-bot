0d94c424ff22eb56885053a2a9897a087f57d961
Mike Dettmer <mike@mikedettmer.com>
2021-05-27 23:55:23 -0400

    feat(): adds pain

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ client.on('message', async message => {
   ) {
     message.channel.startTyping();
     if (message.content.toLowerCase().includes('how much pain are you in?')) {
-      return await message.reply(`i'm at ${[pain]}/100 pain`);
+      await message.reply(`i'm at ${[pain]}/100 pain`);
+      return message.channel.stopTyping();
     }
     if (message.content.toLowerCase().includes('talk to ratbro')) {
       message.channel.stopTyping();
