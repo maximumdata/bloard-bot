13be6cc1a1c3436257edf1fe31b82a97ebb08c1b
Mike Dettmer <mike@mikedettmer.com>
2022-12-06 11:07:30 -0500

    ok

--- commands/nickname.js
+++ commands/nickname.js
@@ -8,8 +8,8 @@ export default {
   execute: async function (message, args, INFERKIT_KEY) {
     if (args.length) {
       const name = args.join(' ');
-      message.channel.startTyping();
       await message.guild.me.setNickname(name);
+      message.channel.startTyping();
       const msg = await aiRequest(message, INFERKIT_KEY, false, true, `My name is ${name}, and i love to`);
       await message.reply(msg)
       message.channel.stopTyping();
