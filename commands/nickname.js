5cc58410797f0ba16c5a17b1420338ed944f65c9
Mike Dettmer <mike@mikedettmer.com>
2022-12-06 10:55:09 -0500

    updates nickname commands

--- commands/nickname.js
+++ commands/nickname.js
@@ -1,11 +1,18 @@
+import aiRequest from '../services/aiRequest';
+
 export default {
   name: 'nickname',
   aliases: ['nick'],
   description: 'change bman\'s display name',
   usage: '',
-  execute: async function (message, args) {
+  execute: async function (message, args, INFERKIT_KEY) {
     if (args.length) {
-      return await message.guild.me.setNickname(args.join(' '));
+      const name = args.join(' ');
+      const msg = await aiRequest(message, INFERKIT_KEY, false, true, `My name is ${name}, and i love to`);
+      await message.channel.startTyping();
+      await message.guild.me.setNickname(name);
+      await message.reply(msg)
+      message.channel.stopTyping();
     }
   }
 };
